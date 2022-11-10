cf0a7489c6dd2c3e290f1adc9bdaa75e91f4b6ad
JDRjuju <94639280+JDRjuju@users.noreply.github.com>
2022-11-10 13:31:01 +0100

    tuto1

--- tuto/pizzeria/api/fat-model/app.js
+++ tuto/pizzeria/api/fat-model/app.js
@@ -4,7 +4,6 @@ const logger = require('morgan');
 
 const usersRouter = require('./routes/users');
 const pizzaRouter = require('./routes/pizzas');
-const authsRouter = require('./routes/auths');
 
 const app = express();
 
@@ -15,6 +14,5 @@ app.use(cookieParser());
 
 app.use('/users', usersRouter);
 app.use('/pizzas', pizzaRouter);
-app.use('/auths', authsRouter);
 
 module.exports = app;
